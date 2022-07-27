78da1b51c5ea69e0e0607241a6f40a5ad8417efb
Fergus  Haywood <fergus.haywood5@gmail.com>
2022-07-27 18:47:51 +1200

    add timer function

--- scripts.js
+++ scripts.js
@@ -123,25 +123,29 @@ let testTimeLeft = 0
 let testErrors = 0
 let testWpm = 0
 let charactersTyped = 0
+let testAccuracy = 0
 let currentWord = ''
 let timer = null
 
 // init mapping stats
 
 currentErrorsEl.innerHTML = testErrors
+currentTimeEl.innerHTML = testTimeLeft
 
 // game functions
 
 let testList = []
 
-function generateTest() {
+function startTest() {
   testList = []
   for (let i = 0; i < testWordSize; i++) {
     let index = Math.floor(Math.random() * testWordSize)
     testList.push(commonWords[index])
   }
+  clearInterval(timer)
 
   wordsArea.innerHTML = ''
+  currentTimeEl.innerHTML = testTimeLeft
 
   testList
     .join(' ')
@@ -151,14 +155,16 @@ function generateTest() {
       span.innerHTML = x
       wordsArea.appendChild(span)
     })
+  startBtn.style.display = 'none'
+  restartBtn.style.display = 'block'
 }
 
 function restartTest() {
   inputArea.value = ''
-  generateTest()
+  startTest()
 }
 
-startBtn.addEventListener('click', generateTest)
+startBtn.addEventListener('click', startTest)
 restartBtn.addEventListener('click', restartTest)
 
 //Getting the current value of the input box
@@ -173,14 +179,22 @@ function processText() {
   let currentInputIndex = currentInputArray.length - 1
   let currentInputChar = currentInputArray[currentInputIndex]
   let currentTestChar = wordsArray[currentInputIndex]
-  let testAccuracy = Math.floor(100 - (testErrors / charactersTyped) * 100)
+
+  testAccuracy = Math.floor(testErrors / charactersTyped)
+
   if (isNaN(testAccuracy)) {
-    testAccuracy = 100
+    testAccuracy = 1
+  }
+
+  if (charactersTyped === 1) {
+    timer = setInterval(updateTimer, 1000)
+  }
+  if (testTimeLeft == 0) {
+    finishGame()
   }
 
   let wordsSpanArr = document.querySelectorAll('span')
-
-  //
+  // need to make backspaces remove characters and if theres an error, remove the error
 
   if (currentInputChar === currentTestChar) {
     wordsSpanArr[currentInputIndex].classList.remove('remove')
@@ -192,9 +206,28 @@ function processText() {
   }
 
   charactersTyped++
-
+  currentWpmEl.innerHTML = getWpm()
   currentErrorsEl.innerHTML = testErrors
-  currentAccuracyEl.innerHTML = `${testAccuracy}%`
+  currentAccuracyEl.innerHTML = `${100 - testAccuracy * 100}%`
   console.log(testAccuracy)
-  console.log(charactersTyped)
+}
+
+function getWpm() {
+  let wpm = (charactersTyped * testAccuracy) / (60 - testTimeLeft)
+  return wpm
+}
+function clearInterval() {
+  testErrors = 0
+  testWpm = 0
+  charactersTyped = 0
+  testTimeLeft = 60
+}
+
+function updateTimer() {
+  testTimeLeft--
+  currentTimeEl.innerHTML = testTimeLeft
+}
+
+function finishGame() {
+  inputArea.disabled = true
 }
